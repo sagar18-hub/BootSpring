692dd4a22024ab891395c1cddde06bf7544ba1d9
abc <abc@example.com>
2020-01-20 16:36:18 +0530

    Dependency Demo

---
+++ DependencyDemo/src/main/java/com/dependency/DependencyDemo/DependencyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dependency.DependencyDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyDemoApplication.class, args);
+	}
+
+}
---
+++ DependencyDemo/src/test/java/com/dependency/DependencyDemo/DependencyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dependency.DependencyDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
