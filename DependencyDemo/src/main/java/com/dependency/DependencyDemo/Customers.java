2b01d9e6a7799499d3021dcc81af2dfafd28dd12
abc <abc@example.com>
2020-01-20 16:45:30 +0530

    Add Customer class

---
+++ DependencyDemo/src/main/java/com/dependency/DependencyDemo/Customers.java
@@ -0,0 +1,44 @@
+package com.dependency.DependencyDemo;
+
+public class Customers {
+	private String custid;
+	private String custname;
+	private String coursename;
+
+	public Customers() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Customers(String custid, String custname, String coursename) {
+		super();
+		this.custid = custid;
+		this.custname = custname;
+		this.coursename = coursename;
+	}
+
+	public String getCustid() {
+		return custid;
+	}
+
+	public void setCustid(String custid) {
+		this.custid = custid;
+	}
+
+	public String getCustname() {
+		return custname;
+	}
+
+	public void setCustname(String custname) {
+		this.custname = custname;
+	}
+
+	public String getCoursename() {
+		return coursename;
+	}
+
+	public void setCoursename(String coursename) {
+		this.coursename = coursename;
+	}
+
+}
