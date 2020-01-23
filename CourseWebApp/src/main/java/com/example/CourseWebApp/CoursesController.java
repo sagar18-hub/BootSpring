e5606af9642d0a5b7fd159bdf510918404ff4914
HP <HP@DESKTOP-QBCNUKF>
2020-01-23 21:29:11 +0530

    Create model and view

--- CourseWebApp/src/main/java/com/example/CourseWebApp/CoursesController.java
+++ CourseWebApp/src/main/java/com/example/CourseWebApp/CoursesController.java
@@ -3,11 +3,10 @@
  */
 package com.example.CourseWebApp;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author HP
@@ -17,13 +16,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CoursesController {
 
 	@RequestMapping("/courses")
-	public String courses(HttpServletRequest req) {
-		HttpSession session = req.getSession();
-		String cname = req.getParameter("cname");
-		System.out.println("the courses is completed" + cname);
+	public ModelAndView courses(@RequestParam("cname") String coursename) {
+		/*
+		 * HttpSession session = req.getSession(); String cname =
+		 * req.getParameter("cname");
+		 */
+		System.out.println("the courses is completed" + coursename);
 		System.out.println("Swami Samarth");
-		session.setAttribute("cname", cname);
-		return "courses.jsp";
+		// session.setAttribute("cname", coursename);
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("cname", coursename);
+		mv.setViewName("courses.jsp");
+		return mv;
 
 	}
 }
