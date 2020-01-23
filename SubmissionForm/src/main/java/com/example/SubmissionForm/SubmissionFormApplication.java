549578e06c468d5046060d29949364680529768b
HP <HP@DESKTOP-QBCNUKF>
2020-01-23 22:10:19 +0530

    new submission form

---
+++ SubmissionForm/src/main/java/com/example/SubmissionForm/SubmissionFormApplication.java
@@ -0,0 +1,13 @@
+package com.example.SubmissionForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmissionFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmissionFormApplication.class, args);
+	}
+
+}
